d0539c3dbb37eb4d14d6ef481a97e0d6c4d9028a
irvinghanchy <kiwipan1218@gmail.com>
2026-07-24 21:06:17 +0800

    chore: add ultimate error catchers for the 500 crash

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -3,6 +3,12 @@ import keystaticConfig from '../../../../keystatic.config';
 
 // 手動將 Vercel 的執行期環境變數 (process.env) 強制注入給 Keystatic，
 // 以繞過 Astro 5 在編譯期遺失環境變數的 Bug。
+console.log("=== KEYSTATIC API INIT ===");
+console.log("hasSecret:", !!process.env.KEYSTATIC_SECRET);
+console.log("hasClientId:", !!process.env.KEYSTATIC_GITHUB_CLIENT_ID);
+console.log("hasClientSecret:", !!process.env.KEYSTATIC_GITHUB_CLIENT_SECRET);
+console.log("==========================");
+
 export const ALL = makeHandler({
   config: keystaticConfig,
   secret: process.env.KEYSTATIC_SECRET,
