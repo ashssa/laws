ed9e1c4195b0a0ac2e8afab864fde841bf9bd6f5
irvinghanchy <kiwipan1218@gmail.com>
2026-07-24 15:43:38 +0800

    fix: override localhost redirect_uri with x-forwarded-host

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -1,6 +1,34 @@
 import { makeHandler } from '@keystatic/astro/api';
 import keystaticConfig from '../../../../keystatic.config';
 
-export const ALL = makeHandler({
+const keystaticHandler = makeHandler({
   config: keystaticConfig,
 });
+
+export const ALL = async (context: any) => {
+  const { request } = context;
+  const forwardedHost = request.headers.get("x-forwarded-host");
+  const forwardedProto = request.headers.get("x-forwarded-proto");
+
+  if (forwardedHost && forwardedProto) {
+    const url = new URL(request.url);
+    url.hostname = forwardedHost;
+    url.protocol = forwardedProto;
+    url.port = "";
+
+    const init: RequestInit = {
+      method: request.method,
+      headers: request.headers,
+    };
+    if (request.method !== 'GET' && request.method !== 'HEAD') {
+      init.body = request.body;
+      // @ts-ignore
+      init.duplex = 'half';
+    }
+
+    const newRequest = new Request(url.toString(), init);
+    return keystaticHandler({ ...context, request: newRequest });
+  }
+
+  return keystaticHandler(context);
+};
