1a4797054b2e03e72b4889649ffc26787ba5fd50
irvinghanchy <kiwipan1218@gmail.com>
2026-07-24 17:49:11 +0800

    fix: restrict middleware to GET requests only

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,26 +5,22 @@ export const onRequest = defineMiddleware(async (context, next) => {
   const forwardedProto = context.request.headers.get("x-forwarded-proto");
 
   if (forwardedHost && forwardedProto) {
-    const url = new URL(context.request.url);
-    url.hostname = forwardedHost;
-    url.protocol = forwardedProto;
-    url.port = "";
+    if (context.request.method === "GET" || context.request.method === "HEAD") {
+      const url = new URL(context.request.url);
+      url.hostname = forwardedHost;
+      url.protocol = forwardedProto;
+      url.port = "";
 
-    const newHeaders = new Headers(context.request.headers);
-    newHeaders.set("host", forwardedHost);
+      const newHeaders = new Headers(context.request.headers);
+      newHeaders.set("host", forwardedHost);
 
-    const init: RequestInit = {
-      method: context.request.method,
-      headers: newHeaders,
-    };
+      const init: RequestInit = {
+        method: context.request.method,
+        headers: newHeaders,
+      };
 
-    if (context.request.method !== "GET" && context.request.method !== "HEAD") {
-      init.body = context.request.body;
-      // @ts-ignore
-      init.duplex = "half";
+      context.request = new Request(url.toString(), init);
     }
-
-    context.request = new Request(url.toString(), init);
   }
 
   return next();
